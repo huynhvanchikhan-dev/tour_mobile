77a7b62d321f6babb92c8ade5aee160c11b85ba5
HUYNH VAN CHI KHAN <huynhkhan91@gmail.com>
2025-01-02 00:38:13 +0700

    new version paymentv2

--- android/app/src/main/kotlin/com/example/tour_booking/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tour_booking
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/utils/api_base_config.dart
+++
@@ -1,3 +0,0 @@
-class ApiBaseConfig {
-  static String IP = "54.252.193.168";
-}
